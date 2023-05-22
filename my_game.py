6868093be5d2472eb8c54190b8afb75ebbaa7abf
IronWoodcutter <a.v.vlasov@ukr.net>
2023-05-22 16:46:54 +0300

    Update my_game.py
    def

--- my_game.py
+++ my_game.py
@@ -3,13 +3,52 @@ from random import randint
 SIZE_N = 5
 SIZE_M = 5
 
+def generate_map(char_x, char_y, char_sign, 
+                 enemy_x, enemy_y, enemy_sign,
+                 exit_x, exit_y, exit_sign, 
+                 size_n=SIZE_N, size_m=SIZE_M):
+
+    world_map = ''
+    for j in range( size_m):
+        row = '|'
+
+        for i in range(size_n):
+            if char_x == i and char_y == j:
+                row += f'{char_sign}|'
+            elif enemy_x == i and enemy_y == j:
+                row += f'{enemy_sign}|'
+            elif exit_x == i and exit_y == j:
+                row += f'{exit_sign}|'
+            else:
+                row += ' |'
+        world_map += f'{row}\n'
+
+    return world_map
+
+
+def move(direction, x, y, size_n=SIZE_N, size_m=SIZE_M):
+    if direction == 'u' and y > 0:
+        y -= 1
+    elif direction == 'd' and y < size_m - 1:
+        y += 1
+    elif direction == 'l' and x > 0:
+        x -= 1
+    elif direction == 'r' and x < size_n - 1:
+        x += 1
+    return x, y
+
+
 char_x = randint(0, SIZE_N - 1)
 char_y = randint(0, SIZE_M - 1)
-
 char_sign = 'X'
+
+enemy_x = randint(0, SIZE_N - 1)
+enemy_y = randint(0, SIZE_M - 1)
+enemy_sign = 'E'
 
 exit_x = randint(0, SIZE_N - 1)
 exit_y = randint(0, SIZE_M - 1)
+exit_sign = 'O'
 
 turns = 0
 
@@ -21,18 +60,10 @@ while True:
     if win_condition:
         char_sign = 'W'
 
-    for j in range(SIZE_M):
-        row = '|'
-
-        for i in range(SIZE_N):
-            if char_x == i and char_y == j:
-                row += f'{char_sign}|'
-            elif exit_x == i and exit_y == j:
-                row += 'O|'
-            else:
-                row += ' |'
-        world_map += f'{row}\n'
-
+    world_map = generate_map(char_x, char_y, char_sign, 
+                 enemy_x, enemy_y, enemy_sign,
+                 exit_x, exit_y, exit_sign, 
+                 size_n=SIZE_N, size_m=SIZE_M)
     print(world_map)
 
     if win_condition:
@@ -41,13 +72,6 @@ while True:
 
     direction = input('Ener direction (u / d / l / r): ')
 
-    if direction == 'u' and char_y > 0:
-        char_y -= 1
-    elif direction == 'd' and char_y < SIZE_M - 1:
-        char_y += 1
-    elif direction == 'l' and char_x > 0:
-        char_x -= 1
-    elif direction == 'r' and char_x < SIZE_N - 1:
-        char_x += 1
+    char_x, char_y = move(direction, char_x, char_y)
 
     turns += 1
